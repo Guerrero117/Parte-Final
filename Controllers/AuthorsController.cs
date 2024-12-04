96bf72bb41e7d69e7fc7c2dece2a8f53ee825eab
Alex5727 <alejandrogrjalva@gmail.com>
2024-12-04 08:09:56 -0800

    first commit

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,36 @@
+﻿using libreria_MOGS.Data.Models.Services;
+using libreria_MOGS.Data.Services;
+using libreria_MOGS.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace libreria_MOGS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private AuthorsService _authorsService;
+
+        public AuthorsController(AuthorsService authorsService)
+        {
+            _authorsService = authorsService;
+        }
+
+        [HttpPost("add-author")]
+        public IActionResult AddAuthor([FromBody] AuthorVM author)
+        {
+            _authorsService.AddAuthor(author);
+            return Ok(); 
+        }
+
+        [HttpGet("get-author-with-books-by-id")]
+        public IActionResult GetAuthorWithBooks(int id)
+        {
+            var response = _authorsService.GetAuthorWithBooks(id);
+            return Ok(response);
+        }
+
+
+    }
+}
